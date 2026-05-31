c48d0247287cc1771842576cac60880680f6232c
VillagerJM <juminham123@gmail.com>
2026-06-01 01:14:04 +0900

    test3umjunsik

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-slate-900 text-white font-sans">
       <h1 className="text-5xl font-bold text-yellow-400 mb-4">
-        두 번째 게임 프로젝트 준비 완료엄준식ㅀ! 🚀
+        두 번째 게임 프로젝트 준비 완료엄준식2!! 🚀
       </h1>
       <p className="text-xl text-slate-300">
         Tailwind CSS와 Firebase 세팅이 완벽하게 끝났습니다.
